d87892aa0f1ab2d2fd0aa2c683350791535c3214
Jcastillo <j.castillomora01@gmail.com>
2023-07-13 10:44:16 -0400

    se usa isort para ordenar los archivos

--- vehiculo/forms.py
+++ vehiculo/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from .models import Vehiculo
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+from .models import Vehiculo
+
 
 class VehiculoForm(forms.ModelForm):
 
@@ -14,5 +15,4 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
     class Meta:
         model = User
-        #fields = ['username', 'email', 'password1', 'password2']
         fields = ('username', 'email', 'password1', 'password2')
